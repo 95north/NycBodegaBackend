77c47ea53a5fdb0971028b54bd7be38c6da6e23f
Tori Sigler <mssigler@gmail.com>
2019-08-16 14:18:33 -0400

    Review #create working!

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,34 @@
 
 class ReviewsController < ApplicationController
 
+    def create
+        @newR = params["newReviewCopy"]
+
+        @review = Review.new
+
+        # byebug
+        @review.user_id = @newR["user_id"]
+        @review.bodega_id= @newR["bodega_id"]
+        # @review.overallreview= @newR["   "]
+        @review.latenight= @newR["latenight"]
+        @review.coffeenumber= @newR["coffeenumber"]
+        @review.coffeetext= @newR["coffeetext"]
+        @review.catnumber= @newR["catnumber"]
+        @review.cattext= @newR["cattext"]
+        @review.sandwichnumber= @newR["sandwichnumber"]
+        @review.sandwichtext= @newR["sandwichtext"]
+        @review.hoursnumber= @newR["hoursnumber"]
+        @review.hourstext= @newR["hourstext"]
+        @review.beernumber= @newR["beernumber"]
+        @review.beertext= @newR["beertext"]
+        @review.snacknumber= @newR["snacknumber"]
+        @review.snacktext= @newR["snacktext"]
+
+        @review.save
+        render json: @review
+    end
+
+
     def index
         @reviews = Review.all
         render json: @sp
